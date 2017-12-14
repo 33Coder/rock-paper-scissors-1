850fd736b5c6ba576bb0438e69254db366528270
Gabriel Aragon <thirtythreebananas@gmail.com>
2017-12-13 22:23:40 -0500

    fully functional with console.logs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 let playerScore = 0;
-  let compScore = 0;
-
+let compScore = 0;
+let game_number = 1;
 
 let computer_play = () => {
 	let randomizer = Math.floor(Math.random()*3);
@@ -23,15 +23,17 @@ let play_round = (playerChoice) => {
   if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
   	check_winner(playerChoice);
   }
-  else
-  {
+  else {
   	console.log("Please enter, rock, paper, or scissors as parameter in play_round()");
   }
+    console.log("1 playerScore=", playerScore, "compScore=", compScore);
+
 };
 
 let check_winner = (playerChoice) => {
   
 	let comp_test = computer_play();
+		//let comp_test = "scissors"; // for testing only
 
 	if (comp_test == playerChoice) {
 		console.log("TIE");
@@ -39,41 +41,64 @@ let check_winner = (playerChoice) => {
 	}
 
 	switch (comp_test) {
+
 		case "rock":
-		console.log(
-		playerChoice == "paper" ?
-		  ("You win, paper beats rock!", playerScore++) : 
-		  ("You lose, rock beats scissors", compScore++)
-		); // this case uses the ternary operator to do the same thing as
-			 // the if/else statements
-		break;	
+		if (playerChoice == "paper") {
+			playerScore++;
+		  console.log("You win, paper beats rock!");
+		  }
+		else {
+    	compScore++;
+      console.log("You lose, rock beats scissors");
+		  }
+		break;
+
 		case "paper":
 		if (playerChoice == "scissors") {
-			console.log("You win, scissors beats paper");
 			playerScore++;
+			console.log("You win, scissors beats paper");	
 		}
 		else {
-			console.log("You lose, paper beats rock");
 			compScore++;
+			console.log("You lose, paper beats rock");	
 		}
 		break;
+
 		case "scissors":
 		if (playerChoice == "rock") {
+			playerScore++;
 			console.log("You win, rock beats scissors");
 		}
 		else {
+			compScore++;
 			console.log("You lose, scissors beats paper");
 		}
 		return playerScore, compScore;
 	}
 };
 
-
-
 let game = () => {
-	let playerChoice = window.prompt("Enter rock, paper, or scissors");
-	play_round(playerChoice);
-  console.log("playerScore=", playerScore, "compScore=", compScore);
+	while (game_number < 6) {	
+		let playerChoice = window.prompt("Enter rock, paper, or scissors");
+		play_round(playerChoice);
+		console.log("playerScore=", playerScore, "compScore=", compScore, "game_number=", game_number);
+	  game_number++;
+	}
+	who_wins();
+};
+
+let who_wins = () => {
+console.log("BOOM");
+let win_statement;
+  if (playerScore > compScore) {
+  	win_statement = "you win!";
+  } else if (playerScore < compScore) {
+  	win_statement = "computer wins.";
+  } else {
+  	win_statement = "it's a tie!";
+  }
+  console.log(`You scored ${playerScore}, the computer scored ${compScore}, ${win_statement}`);
 };
 
 game();
+
